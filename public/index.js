97657229dd67458efc4c3c675baee4ba244dc7fe
obivan <ikolesov20091@bk.ru>
2020-12-15 21:18:52 +0300

    card-name

--- public/index.js
+++ public/index.js
@@ -1,38 +1,39 @@
 const currentData = new Date().toJSON().slice(0, 10).replace(/(\d+)-(\d+)-(\d+)/, '$3-$2-$1');
-
+let listData;
 function showInfographic() {
 
 }
 
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
-        return [
-            {
-                text: "added: " + currentData,
-                color: "green"
-            }
-        ];
-    },
-    "card-detail-badges": function (t, opts) {
-        return [
-            {
-                text: "added: " + currentData,
-                color: "green"
-            }
-        ];
+        return t.card("name")
+            .get("name")
+            .then(function (cardName) {
+                console.log(cardName);
+                opts.attachments.addedData = currentData;
+                return [
+                    {
+                        text: "added: " + currentData,
+                        color: "green"
+                    }
+                ];
+            });
     },
     'board-buttons': function (t, opts) {
-        t.lists("all")
-            .then(function (listData) {
-                console.log(listData);
-                /* return [
-                    {
-                        text: 'infographic',
-                        condition: 'always',
-                        callback: showInfographic
-                    }
-                ] */
-            })
+        return [
+            {
+                text: 'infographic',
+                condition: 'always',
+                callback: showInfographic
+            }
+        ]
 
     }
 });
+
+var t = window.TrelloPowerUp.iframe();
+t.lists("all")
+    .then(function (lists) {
+        console.log(lists);
+        listData = lists;
+    });
