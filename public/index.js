80dfbfe29ef036a1c39d6760f4c97b0b4a926e8d
obivan <ikolesov20091@bk.ru>
2020-12-15 20:55:49 +0300

    debugger

--- public/index.js
+++ public/index.js
@@ -1,16 +1,39 @@
+const currentData = new Date().toJSON().slice(0, 10).replace(/(\d+)-(\d+)-(\d+)/, '$3-$2-$1');
+
+function showInfographic() {
+
+}
+
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
-        let cardAttachments = opts.attachments;
-        return t
-            .card("name")
-            .get("name")
-            .then(function (cardName) {
+        return [
+            {
+                text: "added: " + currentData,
+                color: "green"
+            }
+        ];
+    },
+    "card-detail-badges": function (t, opts) {
+        return [
+            {
+                text: "added: " + currentData,
+                color: "green"
+            }
+        ];
+    },
+    'board-buttons': function (t, opts) {
+        debugger
+        t.board("all")
+            .then(function (boardData) {
+                console.log(boardData);
                 return [
                     {
-                        text: "Added: " + new Date().toJSON().slice(0, 10).replace(/(\d+)-(\d+)-(\d+)/, '$3-$2-$1'),
-                        color: "green"
-                    },
-                ];
-            });
-    },
+                        text: 'infographic',
+                        condition: 'always',
+                        callback: showInfographic
+                    }
+                ]
+            })
+
+    }
 });
