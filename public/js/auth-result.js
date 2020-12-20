ad9cc230a38ae03970488db661dabd0281e4391a
obivan <ikolesov20091@bk.ru>
2020-12-20 13:37:05 +0300

    token

--- public/js/auth-result.js
+++ public/js/auth-result.js
@@ -1,10 +1,8 @@
 const token = window.location.hash.substring(7);
-localStorage.setItem('token', token);
-/* debugger
-if (window.opener) {
-    window.opener.authorize(token);
-} else {
-    localStorage.setItem('token', token);
-} */
+
+let key = "token";
+
+localStorage.setItem(key, token);
+
 
 setTimeout(function () { window.close(); }, 1000);
