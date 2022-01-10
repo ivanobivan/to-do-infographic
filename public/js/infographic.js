1078597c0572017bf13cb3acca27e8300f614714
Ivan.Kolesov <ikolesov@bivgroup.ru>
2022-01-10 12:31:59 +0300

    try to open by html path

--- public/js/infographic.js
+++ public/js/infographic.js
@@ -120,24 +120,97 @@ function createExplainDiv(startDate, endDate) {
 
 async function createReportHandler(event) {
     try {
-        debugger
         const token = await t.loadSecret(PRIVATE_TOKEN_PATH);
         const settings = await t.get("board", "private", SETTINGS_KEY)
         const data = await getDataForInfographic(token, settings);
+        debugger
 
-        t.closeModal();
-
-        t.modal({
-            url: './new-year-report.html',
-            fullscreen: false,
-            callback: () => {console.log("123")},
-            title: 'Новогодний отчет',
-            args: {data, settings}
-        });
-
+        const nodeList = reportDataPreparation(data, settings);
+        const tab = window.open("https://to-do-infographic.vercel.app/new-year-report.html");
+        if(tab) {
+            const main = tab.document.querySelector("main");
+            if(main) {
+                main.append(...nodeList);
+            }
+        }
     } catch (err) {
         console.error(err)
     }
+}
+
+function reportDataPreparation(data, settings) {
+    const { headers, body } = data;
+    const { startDate, endDate, list } = settings;
+
+    const resultList = [];
+
+    headers.forEach((header, i) => {
+        const section = document.createElement("section");
+        section.className = "container";
+
+        const headerNode = document.createElement("header");
+        headerNode.innerText = header.toUpperCase();
+
+        const container = document.createElement("div");
+        container.className = "card-container";
+
+        const cardList = body[i];
+        for (let i = 0; i < cardList.length; i++) {
+            const card = cardList[i];
+            if (!card.closed) {
+                continue;
+            }
+
+            const div = document.createElement("div");
+            div.className = "card";
+
+            div.onmouseover = (event) => {
+                event.target.style.cssText = "transform: scale(1.2); opacity: 1";
+            };
+
+            div.onmouseout = (event) => {
+                event.target.style.cssText = "transform: scale(1); opacity: 0.7";
+            };
+
+            div.innerText = `${card.name}\n${new Date(card.date).toDateString()}`;
+
+            container.append(div);
+        }
+
+        const sumNode = document.createElement("div");
+        sumNode.className = "sum";
+        sumNode.innerText = `Всего выполнено ${container.childElementCount} элементов`;
+
+        const imgContainerTop = document.createElement("div");
+        imgContainerTop.className = "img-container";
+
+        const imgContainer = document.createElement("div");
+        imgContainer.className = "img-container";
+
+        const imgTop = document.createElement("img");
+        const img = document.createElement("img");
+
+        if (header.includes("book")) {
+            imgTop.src = "./public/img/book-2.png";
+            img.src = "./public/img/book.png";
+        } else if (header.includes("film")) {
+            imgTop.src = "./public/img/film-2.png";
+            img.src = "./public/img/film.png";
+        } else if (header.includes("game")) {
+            imgTop.src = "./public/img/game-2.png";
+            img.src = "./public/img/game.png";
+        } else if (header.includes("series")) {
+            imgTop.src = "./public/img/series-2.png";
+            img.src = "./public/img/series.png";
+        }
+        imgContainerTop.append(imgTop, imgTop.cloneNode());
+        imgContainer.append(img, img.cloneNode());
+
+        section.append(headerNode, imgContainerTop, sumNode, container, imgContainer);
+        resultList.push(section);
+    });
+
+    return resultList;
 }
 
 function generateUniqId() {
